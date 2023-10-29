59bdbb2d7973a9c83620dc19f8c8901bbefac3c9
chunkymunky0 <44077280+chunkymunky0@users.noreply.github.com>
2023-10-29 19:36:41 -0400

    Put code back in impossibleBot.py from  main

--- impossibleBot.py
+++ impossibleBot.py
@@ -1,5 +1,6 @@
 from gui import *
 from ticTacToe import *
+import math
 
 def spaceAvailable(r, c):
     if [position] == '0':
@@ -33,7 +34,34 @@ def impBotMove():
     insertLetter(computer, bestMove)
     return 
 
-
+def minimax(board, isMaximizing):
+    if checkWhoWon(computer):
+        return 1 
+    elif checkWhoWon(player):
+        return -1 
+    elif checkDraw():
+        return 0
+        
+    if isMaximizing:
+        bestScore = -800
+        for key in board.keys():
+            if board[key] == ' ':
+                board[key] = computer 
+                score = minimax(board, False)
+                board[key] = ' '
+                if score > bestScore:
+                    bestScore = score
+        return bestScore 
+    else:
+        bestScore = 800 
+        for key in board.keys():
+            if board[key] == ' ':
+                board[key] = player 
+                score = minimax(board, True)
+                board[key] = ' '
+                if score < bestScore:
+                    bestScore = score 
+        return bestScore
 
 
 while not checkWin():
