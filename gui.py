e6ae3f1ac3fd9313a22294a0cc59da3981ac8c31
Junior Gamarra <108636645+gamarrju@users.noreply.github.com>
2023-10-29 19:08:53 -0400

    Update gui.py

--- gui.py
+++ gui.py
@@ -5,7 +5,7 @@ class TicTacToeBoard(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Tic-Tac-Toe Game")
-        self.configure(bg="#3498db")  # blue
+        self.configure(bg="#3498db")  # Set the background color to a shade of blue
         self._create_board_grid()
 
     def _create_board_grid(self):
@@ -18,18 +18,24 @@ class TicTacToeBoard(tk.Tk):
                     master=self,
                     relief=tk.RAISED,
                     borderwidth=1,
-                    bg="#ecf0f1"  #light gray
+                    bg="#ecf0f1"  # Set the background color of the squares to a light gray
                 )
                 frame.grid(row=row, column=col, padx=5, pady=5, sticky="nsew")
 
-                label = tk.Label(
+                button = tk.Button(
                     master=frame,
                     text="",
                     font=font.Font(size=36, weight="bold"),
-                    fg="#2c3e50",  # dark gray
-                    bg="#ecf0f1"
+                    fg="#2c3e50",  # Set the text color to a dark gray
+                    bg="#ecf0f1"  # Set the background color of the buttons to match the squares
                 )
-                label.pack(fill=tk.BOTH, expand=True)
+                button.pack(fill=tk.BOTH, expand=True)
+
+                # Bind a callback function (click_button) to the button click event
+                button.bind("<Button-1>", self.click_button)
+
+    def click_button():
+        pass
 
 def main():
     board = TicTacToeBoard()
